54036b6c467dc16c86ff34fbd4b1f1f7b4c9ca08
cholokei <leesl0416@naver.com>
2015-12-18 19:16:00 +0900

    Fix build errors in CyanogenMod modified:   arch/arm/mach-msm/bam_dmux.c modified:   drivers/misc/sec_misc.c modified:   drivers/misc/sec_param.c modified:   drivers/video/msm/mdss/mdnie_tft_msm8x26.c modified:   net/bluetooth/hci_conn.c
    Signed-off-by: cholokei <leesl0416@naver.com>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -394,6 +394,7 @@ void hci_le_start_enc(struct hci_conn *conn, __le16 ediv, __u8 rand[8],
 }
 EXPORT_SYMBOL(hci_le_start_enc);
 
+/*
 void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[16])
 {
 	struct hci_dev *hdev = conn->hdev;
@@ -409,6 +410,7 @@ void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[16])
 	hci_send_cmd(hdev, HCI_OP_LE_LTK_REPLY, sizeof(cp), &cp);
 }
 EXPORT_SYMBOL(hci_le_ltk_reply);
+*/
 
 void hci_le_ltk_neg_reply(struct hci_conn *conn)
 {
